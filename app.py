9ea0574e43ef4f4ba71ff08ed69be6c7b8097271
tusharchaudharryy <chaudharytushar477@gmail.com>
2025-08-24 13:58:58 +0530

    New Commiys

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify, render_template
 import os
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS, cross_origin
 from cnnClassifier.utils.common import decodeImage
 from cnnClassifier.pipeline.prediction import PredictionPipeline
